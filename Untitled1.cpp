e4318b98c2ae36253d6b4e6f1b7c34ef4255b814
ahmad12345612345 <tamania50@yahoo.com>
2020-01-13 10:58:06 +0500

    Juggar Syster

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,5 +2,35 @@
 using namespace std;
 int main()
 {
+	cout<<"                                               PRIME NUMBER "<<endl;
+	int number;
+	cout<<"Enter ur number: ";
+	cin>>number;
+	cout<<"Prime numbers between 0 and "<<number<<" are: ";
+	if(number==2)
+	{
+		cout<<"2"<<endl;
+	}
 	
+	
+	if((number==4)||(number==3))
+	{
+		cout<<"2"<<endl;
+		cout<<"3"<<endl;	
+	}
+	if(number>=5){
+		cout<<"2"<<endl;
+		cout<<"3"<<endl;
+		cout<<"5"<<endl;
+	}
+	for(int i=0;i<=number;i++){
+	
+		if((i%2!=0)&&(i%3!=0)&&(i%5!=0)){
+			cout<<i<<endl;
+		}
+		else{
+		}
+	}
+	
+	return 0;
 }
